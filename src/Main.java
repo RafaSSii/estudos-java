3ef73de6a03b7eccdc36f42face0ccaeb9ae14b5
Rafael <rafarsk78@gmail.com>
2025-08-03 21:18:22 -0300

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Digite por qual número quer começar");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Digite por qual número quer terminar");
+        int num2 = scanner.nextInt();
+        
+        for (int i = num1; i <= num2; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            } else {
+                System.out.println(" ");
+            }
+        }
+    }
+}
