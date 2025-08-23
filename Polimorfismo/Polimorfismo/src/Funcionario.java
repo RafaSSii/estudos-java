eb27ac404f82517cd3451a6e85c5a6a463f6ca82
Rafael Santos <149257772+RafaSSii@users.noreply.github.com>
2025-08-23 12:17:35 -0300

    Estudo Polimorfismo

---
+++ Polimorfismo/Polimorfismo/src/Funcionario.java
@@ -0,0 +1,6 @@
+public class Funcionario extends Pessoa {
+    @Override
+    public void falar(){
+        System.out.println("Oi, eu sou um funcionário");
+    }
+}
---
+++ Polimorfismo/Polimorfismo/src/Pessoa.java
@@ -0,0 +1,9 @@
+public class Pessoa {
+    public void falar(){
+        System.out.println("Oi");
+    }
+
+    public void falar(String texto){
+        System.out.println("oi "+texto);
+    }
+}
