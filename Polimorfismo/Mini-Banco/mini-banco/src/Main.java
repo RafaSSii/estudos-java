342efdd9804942d204bd868b5475f0ea08bf6005
Rafael Santos <149257772+RafaSSii@users.noreply.github.com>
2025-08-27 13:10:33 -0300

    Polimorfismo Hospital

--- Polimorfismo/Mini-Banco/mini-banco/src/Main.java
+++ Polimorfismo/Mini-Banco/mini-banco/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Paciente paciente = new Paciente("1234", "Unimed", "Rafael", 22, "1234567");
+        System.out.println("Informações do Paciente");
+        Pessoa paciente = new Paciente("1234", "Unimed", "Rafael", 22, "1234567");
         paciente.imprimirInfo();
+
+        System.out.println("\nInformações do Enfermeiro");
+        Pessoa enfermeiro = new Enfermeiro(12, "Coleta", "789", "Luiza", 21, "4567");
+        enfermeiro.imprimirInfo();
     }
 }
